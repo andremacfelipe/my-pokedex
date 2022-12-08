9e80b21537ff29c972c72b4a17ec713f226172dd
André Felipe <andremacielfelipe@gmail.com>
2022-12-08 19:50:26 -0300

    stylized Pokemon card types

--- src/components/PokemonCard/PokemonCardTypesIcons.jsx
+++ src/components/PokemonCard/PokemonCardTypesIcons.jsx
@@ -41,8 +41,9 @@ const PokemonCardTypesIcons = {
     water:waterIcon
 }
 const pokemonCardIconImageStyle = {
-    width:"16px",
-    height:"16px"
+    width:"18px",
+    height:"18px",
+    marginLeft:"3px"
 }
 
 const PokemonCardIconImage = ({pokemonType}) => {
