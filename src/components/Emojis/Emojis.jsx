c19c317191e674551e6e6ee2f83b1f7686d8f7e5
André Felipe <andremacielfelipe@gmail.com>
2022-12-12 00:01:28 -0300

    Basic page layout created

---
+++ src/components/Emojis/Emojis.jsx
@@ -0,0 +1,26 @@
+const HeartEmoji = () => {
+    return(
+        <span role="img" aria-label="heart" >❤️</span>
+    )
+}
+
+const ShieldEmoji = () => {
+    return (
+        <span role="img" aria-label="shield" >🛡️</span>
+    )
+}
+
+const SwordEmoji = () => {
+    return (
+        <span role="img" aria-label="sword" >⚔️</span>
+    )
+}
+
+const FootEmoji = () => {
+    return (
+        <span role="img" aria-label="foot" >🦶</span>
+    )
+}
+
+
+export {HeartEmoji,ShieldEmoji,SwordEmoji,FootEmoji}
--- src/components/PokemonOverviewCard/PokemonOverviewCard.jsx
+++ src/components/PokemonOverviewCard/PokemonOverviewCard.jsx
@@ -1,6 +1,12 @@
 import './style.css'
 import PokemonCard from '../PokemonCard/PokemonCard'
 
+import PokemonCardIconImage from '../PokemonCard/PokemonCardTypesIcons'
+import PokemonCardTypesColors, {PokemonCardTypesBackgroundColor} from '../PokemonCard/PokemonCardTypesColors'
+
+import { FootEmoji,HeartEmoji,ShieldEmoji,SwordEmoji } from '../Emojis/Emojis' 
+
+
 const PokemonOverviewCard = ({pokemon}) => {
     
 
@@ -12,13 +18,41 @@ const PokemonOverviewCard = ({pokemon}) => {
             pokemonBaseAttack,
             pokemonBaseDefense,
             pokemonBaseSpeed,
+            pokemonId
         } = pokemon
 
 
 
     return (
         
-        <PokemonCard pokemon={pokemon}/>
+        <article className='PokemonOverviewCard'>
+            <div className="pokemonIdBox">
+                <h2 className='pokemonId'>{`#${pokemonId}`}</h2>
+            </div>
+            <section className="pokemonPicAndNameBox">
+                <div className="pokemonPicBox">
+                    <img src={pokemonCardPicSrc} alt="pokemonCardPic" />
+                </div>
+                <div className="PokemonNameBox">
+                    <h1>{pokemonName}</h1>
+                </div>
+                
+
+            </section>
+
+            <div className="pokemonOverviewCardInfo">
+                
+                <div className="pokemonCombatStats">
+                    <h2 className='pokemonCombatStatsTitle'>Stats</h2>
+                    <p className='pokemonBaseHp'>HP<HeartEmoji />: {pokemonBaseHp}</p>
+                    <p className='pokemonBaseAttack'>ATTACK<SwordEmoji/>: {pokemonBaseAttack}</p>
+                    <p className='pokemonBaseDefense'>DEFENSE<ShieldEmoji/>: {pokemonBaseDefense}</p>
+                    <p className="pokemonBaseSpeed">SPEED<FootEmoji/>: {pokemonBaseSpeed}</p>
+                </div>
+                
+            </div>
+            
+        </article>
     )
 
 }
